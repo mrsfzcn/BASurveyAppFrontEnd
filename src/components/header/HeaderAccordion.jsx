961e23eb80908ee9175ff397b5a02f8c8ab7a90c
mertnamsal <namsalmert@gmail.com>
2023-07-25 17:22:50 +0300

    task-678 kullanici ekle sayfasi son düzenlemeler yapildi.

--- src/components/header/HeaderAccordion.jsx
+++ src/components/header/HeaderAccordion.jsx
@@ -33,14 +33,14 @@ function HeaderAccordion({ items }) {
   return (
     <div className="relative">
       <div
-        className="flex items-center justify-center xldektop:w-14 xldektop:h-14 w-10 h-10 border rounded-full bg-secondColor text-gray-900 font-semibold cursor-pointer"
+        className="flex items-center justify-center xldektop:w-14 xldektop:h-14 w-10 h-10 border-black rounded-full bg-secondColor text-gray-900 font-semibold cursor-pointer"
         onClick={handleClick}
       >
         <AiOutlineUser size={24} />
       </div>
       {isOpen && (
         <div className="absolute mt-3 right-0 w-44 z-10 xldektop:w-56">
-          <ul className="flex flex-col border bg-secondColor rounded-md px-4 pt-3 pb-1">
+          <ul className="flex flex-col border-black bg-secondColor rounded-md px-4 pt-3 pb-1">
             {render}
           </ul>
         </div>
--- src/pages/user/UserRegistration.jsx
+++ src/pages/user/UserRegistration.jsx
@@ -4,100 +4,161 @@ import Input from "../../components/Input";
 import Layout from "../../components/Layout";
 import "./userRegistration.css";
 import Dropdown from "../../components/Dropdown";
-import AuthService from "../../services/AuthService"
+import AuthService from "../../services/AuthService";
 import Alert from "../../components/Alert";
+import BreadCrumbs from "../../components/BreadCrumbs";
+import MultiDropdown from "../../components/MultiDropdown";
 
 const UserRegistration = () => {
-  
-  const[user,setUser] = useState({
+  const [user, setUser] = useState({
     password: "",
     firstName: "",
     lastName: "",
-    roles:[
-      
-    ]
+    roles: [],
   });
   const [alert, setAlert] = useState({ type: "", message: "" });
-  
   const [selection, setSelection] = useState(null);
-  const options =[
-    {label:"ADMIN", value:"ADMIN"},
-    {label:"MANAGER", value:"MANAGER"},
-    {label:"STUDENT", value:"STUDENT"},
-    {label:"MASTER_TRAINER", value:"MASTER_TRAINER"},
-    {label:"ASSISTANT_TRAINER", value:"ASSISTANT_TRAINER"}, 
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const options = [
+    { label: "ADMIN", value: "ADMIN" },
+    { label: "MANAGER", value: "MANAGER" },
+    { label: "STUDENT", value: "STUDENT" },
+    { label: "MASTER_TRAINER", value: "MASTER_TRAINER" },
+    { label: "ASSISTANT_TRAINER", value: "ASSISTANT_TRAINER" },
   ];
-  const onChange = (option) => {
-    setSelection(option);
-    setUser({...user,roles:[option.value]});
+  const handleSelectedOptionsChange = (updatedOptions) => {
+    setSelectedOptions(updatedOptions);
+    setUser({ ...user, roles: [updatedOptions.value] });
   };
-
-  const handleCreate = (event) =>{
+  const handleCreate = (event) => {
     event.preventDefault();
-    console.log(user);
-    console.log(typeof user.roles);
-    if(user.firstName.length < 2){
+    if (user.firstName.length < 2) {
       setAlert({ type: "error", message: "Ad 2 karakterden uzun olmalıdır." });
       return;
     }
-    if(user.lastName.length < 2){
-      setAlert({ type: "error", message: "Soyad 2 karakterden uzun olmalıdır." });
+    if (user.lastName.length < 2) {
+      setAlert({
+        type: "error",
+        message: "Soyad 2 karakterden uzun olmalıdır.",
+      });
       return;
     }
-    if(user.password.length < 8){
-      setAlert({ type: "error", message: "Şifre 8 karakterden uzun olmalıdır." });
+    if (user.password.length < 8) {
+      setAlert({
+        type: "error",
+        message: "Şifre 8 karakterden uzun olmalıdır.",
+      });
       return;
     }
-    if(user.roles.length<1){
+    if (user.roles.length < 1) {
       setAlert({ type: "error", message: "Rol seçiniz." });
       return;
     }
-    
-    AuthService.register(user).then(()=>{
-      setAlert({ type: "success", message: "Etiket başarıyla güncellendi." });
-    })
-    .catch((error)=>{
 
-      setAlert({ type: "error", message: "HATA" });
-    })
+    AuthService.register(user)
+      .then(() => {
+        setAlert({ type: "success", message: "Etiket başarıyla güncellendi." });
+      })
+      .catch((error) => {
+        setAlert({
+          type: "error",
+          message:
+            "Beklenmeyen bir hata meydana geldi. Lütfen daha sonra tekrar deneyiniz.",
+        });
+      });
+  };
+  const header = { header: "Kullanici Ekle", href: "/kullanici/ekle" };
 
-  }
+  const subtitle = [
+    {
+      title: "Anasayfa",
+      href: "/adminhome",
+    },
+    {
+      title: "Kullanıcı İşlemleri",
+      href: "/kullanici",
+    },
+    {
+      title: "Kullanici Ekle",
+      href: "/kullanici/ekle",
+    },
+  ];
 
   return (
     <Layout>
       <div className="flex flex-col bg-[#E5E5E5] h-full">
-        <div className="p-5 text-start">
-          Bitmesi bekleniyor
-        </div>
+        <BreadCrumbs header={header} subtitle={subtitle} />
         <div className="class1  flex justify-center align-center">
           <div className="class2  bg-[#F1F1F1] flex  justify-center align-center m-auto ">
             <div className="class3  bg-[#FEFEFE] m-auto  flex flex-col justify-center gap-12 ">
-              <form onSubmit={handleCreate} className=" flex flex-col justify-center gap-12">
-              <div className="flex flex-col gap-12 justify-center items-center">
-               
-                <Input placeholder="Ad" half required onChange={(e=>setUser({...user,firstName: e.target.value}))}/>
-                <Input placeholder="Soyad" half required onChange={(e=>setUser({...user,lastName: e.target.value}))}/>
-                <Input placeholder="Şifre" half required onChange={(e=>setUser({...user,password: e.target.value}))}/>
-                <div className="w-1/2 flex justify-start">
-                  <Dropdown options={options} onChange={onChange} value={selection}/>
+              <form
+                onSubmit={handleCreate}
+                className=" flex flex-col justify-center gap-12"
+              >
+                <div className="flex flex-col gap-12 justify-center items-center">
+                  <Input
+                    placeholder="Ad"
+                    half
+                    required
+                    onChange={(e) =>
+                      setUser({ ...user, firstName: e.target.value })
+                    }
+                  />
+                  <Input
+                    placeholder="Soyad"
+                    half
+                    required
+                    onChange={(e) =>
+                      setUser({ ...user, lastName: e.target.value })
+                    }
+                  />
+                  <Input
+                    placeholder="Şifre"
+                    half
+                    required
+                    onChange={(e) =>
+                      setUser({ ...user, password: e.target.value })
+                    }
+                  />
+                  <div className="w-1/2 flex justify-start items-center gap-5 ">
+                    {/* <Dropdown options={options} onChange={onChange} value={selection}/> */}
+                    <MultiDropdown
+                      options={options}
+                      selectedOptions={selectedOptions}
+                      onChange={handleSelectedOptionsChange}
+                    />
+                    <div className="flex flex-wrap gap-4 items-center justify-center">
+
+                    
+                    
+                    {selectedOptions.map((option) => (
+                      <span
+                        className="flex justify-center items-center border-2 px-2 py-1 rounded-md bg-[#64E9B1] text-black text-sm font-semibold"
+                        key={option.value}
+                      >
+                        {option.label}
+                      </span>
+                    ))}
+                    </div>
+                  </div>
+                  {alert.type && (
+                    <Alert type={alert.type} message={alert.message} />
+                  )}
                 </div>
-                {alert.type && <Alert type={alert.type} message={alert.message} />}
-              </div>
-              
-              <div className="flex justify-center gap-7 flex-wrap">
-                <Button className="" primary rounded bold>
-                  KAYDET
-                </Button>
-                <Button className="" secondary rounded bold>
-                  VAZGEÇ
-                </Button>
-              </div>
+
+                <div className="flex justify-center gap-7 flex-wrap">
+                  <Button className="" primary rounded bold>
+                    KAYDET
+                  </Button>
+                  <Button className="" secondary rounded bold>
+                    VAZGEÇ
+                  </Button>
+                </div>
               </form>
             </div>
           </div>
         </div>
       </div>
-      
     </Layout>
   );
 };
