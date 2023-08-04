7cf63bb43b93645b860bfecca03d8414d3dc4ae9
MuhammedFurkanTurkmen <105326305+MFurkanTurkmen@users.noreply.github.com>
2023-08-04 17:59:56 +0300

    private route dosyası eklendi

---
+++ src/utils/encrypt.js
@@ -0,0 +1,13 @@
+import CryptoJS from "crypto-js";
+
+const SECRET_KEY =
+  "46b9dd2b0ba88d13233b3feb743eeb243fcd52ea62b81b82b50c27646ed5762fd75dc4ddd8c0f200cb05019d67b592f6fc821c49479ab48640292eacb3b7c4be";
+
+export function encrypt(text) {
+  return CryptoJS.AES.encrypt(text, SECRET_KEY).toString();
+}
+
+export function decrypt(ciphertext) {
+  const bytes = CryptoJS.AES.decrypt(ciphertext, SECRET_KEY);
+  return bytes.toString(CryptoJS.enc.Utf8);
+}
