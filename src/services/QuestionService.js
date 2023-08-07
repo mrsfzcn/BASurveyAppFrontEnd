98d2e2be18eab1874ed5a5c57e2dee9d3f66550b
enesdoganoglu <117980244+enesdoganoglu@users.noreply.github.com>
2023-08-08 02:54:17 +0300

    Soru ekle sayfası backend bağlantısı yapıldı.

--- src/services/QuestionService.js
+++ src/services/QuestionService.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const GETALL = `http://localhost:8090/api/v1/questions/findallquestion`;
 const DELETE = `http://localhost:8090/api/v1/questions/deletequestionbyid/`;
-const GET_QUESTION_BY_ID = "http://localhost:8090/api/v1/questions/findById/"
+const GET_QUESTION_BY_ID = "http://localhost:8090/api/v1/questions/findById/";
+const GET_ALL_QUESTION_TAGS = "http://localhost:8090/api/v1/questiontags";
+const CREATE_QUESTION = "http://localhost:8090/api/v1/questions";
 
 class QuestionService {
 
@@ -34,6 +36,24 @@ class QuestionService {
         },
       };
       return await axios.get(GET_QUESTION_BY_ID + id, config);
+    }
+    async getAllQuestionTags(id) {
+      const token = localStorage.getItem("token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      return await axios.get(GET_ALL_QUESTION_TAGS , config);
+    }
+    async createQuestions(createQuestion) {
+      const token = localStorage.getItem("token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      return await axios.post(CREATE_QUESTION ,createQuestion, config);
     }
 }
 export default new QuestionService();
