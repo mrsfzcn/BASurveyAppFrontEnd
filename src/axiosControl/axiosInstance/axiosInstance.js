7771acfbae3ea7f4059e297f0b7b460a00d98681
mrsfzcn <mrsfzcn@gmail.com>
2023-11-16 17:21:30 +0300

    cleanup axiosInstance file

--- src/axiosControl/axiosInstance/axiosInstance.js
+++ src/axiosControl/axiosInstance/axiosInstance.js
@@ -48,42 +48,9 @@ const axiosInstance = (baseURL) => {
 
 const baseURL = "http://localhost:8090/api/v1";
 
-const axiosInstanceAuth = axiosInstance(`${baseURL}/auth`);
-const axiosInstanceBranch = axiosInstance(`${baseURL}/branch`);
-const axiosInstanceCourse = axiosInstance(`${baseURL}/course`);
-const axiosInstanceCourseGroup = axiosInstance(`${baseURL}/course-group`);
-const axiosInstanceMainTags = axiosInstance(`${baseURL}/main-tags`);
-const axiosInstanceQuestions = axiosInstance(`${baseURL}/questions`);
-const axiosInstanceQuestionTags = axiosInstance(`${baseURL}/question-tags`);
-const axiosInstanceQuestionTypes = axiosInstance(`${baseURL}/question-types`);
-const axiosInstanceResponses = axiosInstance(`${baseURL}/responses`);
-const axiosInstanceRole = axiosInstance(`${baseURL}/role`);
-const axiosInstanceStudent = axiosInstance(`${baseURL}/student`);
-const axiosInstanceStudentTag = axiosInstance(`${baseURL}/student-tag`);
-const axiosInstanceSurvey = axiosInstance(`${baseURL}/survey`);
-const axiosInstanceSurveytag = axiosInstance(`${baseURL}/surveytag`);
-const axiosInstanceTrainer = axiosInstance(`${baseURL}/trainer`);
-const axiosInstanceTrainerTag = axiosInstance(`${baseURL}/trainer-tag`);
-const axiosInstanceUser = axiosInstance(`${baseURL}/user`);
-const axiosInstanceUserTag = axiosInstance(`${baseURL}/user-tag`);
+const axiosInstanceGlobal = axiosInstance(`${baseURL}`);
+
 
 export {
-  axiosInstanceAuth,
-  axiosInstanceBranch,
-  axiosInstanceCourse,
-  axiosInstanceCourseGroup,
-  axiosInstanceMainTags,
-  axiosInstanceQuestions,
-  axiosInstanceQuestionTags,
-  axiosInstanceQuestionTypes,
-  axiosInstanceResponses,
-  axiosInstanceRole,
-  axiosInstanceStudent,
-  axiosInstanceStudentTag,
-  axiosInstanceSurvey,
-  axiosInstanceSurveytag,
-  axiosInstanceTrainer,
-  axiosInstanceTrainerTag,
-  axiosInstanceUser,
-  axiosInstanceUserTag,
+  axiosInstanceGlobal,
 };
