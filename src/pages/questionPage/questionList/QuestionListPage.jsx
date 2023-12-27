8fef3a3f6483b6aaf31ce6bfaa9186efcba3888b
mrsfzcn <mrsfzcn@gmail.com>
2023-12-27 23:35:29 +0300

    Task-1001 #1001 #### QuestionListPage güncellendi.
    - Sınıfa DataTable entegre edildi.
    - Artık pointer ile id-soru-soru tipi ve etikete göre sıralama yapılabiliyor.
    
    - Alertler tostify olarak güncellendi.

--- src/pages/questionPage/questionList/QuestionListPage.jsx
+++ src/pages/questionPage/questionList/QuestionListPage.jsx
@@ -1,16 +1,105 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import "./questionListPage.css";
-import { Link, useNavigate } from "react-router-dom";
-import Table from '../../../components/Table/Table';
+import { useNavigate } from "react-router-dom";
 import Layout from '../../../components/Layout'
 import QuestionService from '../../../services/QuestionService';
 import BreadCrumbs from '../../../components/BreadCrumbs'
+import DataTable from 'react-data-table-component';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import EditIcon from '../../user/AllUsers/svg/edit-svg';
+import DeleteIcon from '../../user/AllUsers/svg/delete-svg';
 
 function QuestionListPage() {
 
-  const [surveys, setSurveys] = useState([]);
+  const successNotify = (string) => toast.success(string);
+  const errorNotify = (string)=> toast.error(string);
+  const warnNotify = (string)=> toast.warn(string);
 
-  const header = ["No", "Soru", "Soru Tipi", "Soru Etiketleri","Seçenekler"];
+  const [surveys, setSurveys] = useState([ //bunun adı neden surveys ? question ile çalışmıyor muyuz?
+    {
+      no: "",
+      soru: "",
+      tipi: "",
+      etiketler: "",
+      secenekler: "",
+    }
+  ]);
+
+  const header = [
+    {
+      name: 'No',
+      selector: row => row.questionOid,
+      sortable: true,
+    }, 
+    {
+      name: 'Soru',
+      selector: row => {
+        if(row.questionType == "Matriks")
+        return row.questionString.split(" $$ ").join(", ")
+        return row.questionString
+      },
+      sortable: true,
+    }, 
+    {
+      name: "Soru Tipi",
+      selector: row =>  row.questionType,
+      sortable: true,
+    }, 
+    {
+      name: "Etiketler",
+      selector: row => {
+        console.log(row.questionTags);
+        return row.questionTags},
+      sortable: true,
+    },
+    {
+      name: "Seçenekler",
+      selector: row => {
+        if(Array.isArray(row.questionOptions)){
+          return row.questionOptions.join(", ")
+        }
+        return row.questionOptions
+      },
+      sortable: true,
+    },
+    {
+      name: "İşlemler",
+      selector: row => {
+        return <>
+        <div className='flex gap-2'>
+        <button onClick={()=> deleteQuestion(row.questionOid)}>
+          <DeleteIcon className='delete-btn'/>
+        </button>
+        <button onClick={()=> handleEditClick(row)}>
+          {console.log(row)}
+          <EditIcon className='edit-btn'/>
+        </button>
+        </div>
+        </>
+        
+      },
+      sortable: false,
+    }
+  ];
+
+  const deleteQuestion = async(questionOid) => {
+    let response;
+    try{
+      response = await QuestionService.delete(questionOid);
+      successNotify(questionOid + " No'lu soru başarıyla silindi");
+    } catch(error){
+      errorNotify(error.response.data.exceptionMessage);
+    }
+    if(response.data){
+      setSurveys(
+        surveys.filter(survey => survey.questionOid != questionOid)
+        )
+    }
+  }
+
+  console.log(surveys);
 
   // To delete selected question
   const deleteTableRows = async (index, rowData) => {
@@ -18,17 +107,17 @@ function QuestionListPage() {
     try {
       response = await QuestionService.delete(rowData.questionOid);
     } catch (error) {
-      alert(error.response.data.exceptionMessage);
+      errorNotify(error.response.data.exceptionMessage);
     }
 
     if (response.data) {
-      alert(rowData.questionOid + " No'lu soru başarıyla silindi");
+      successNotify(rowData.questionOid + " No'lu soru başarıyla silindi");
     console.log(response);
     const rows = [...surveys];
     rows.splice(index, 1);
     setSurveys(rows);
     } else {
-    alert("bir hata meydana geldi");
+      errorNotify("bir hata meydana geldi");
     }
   };
 
@@ -44,7 +133,7 @@ function QuestionListPage() {
     const fetchData = async () => {
       try {
         const response = await QuestionService.list()
-        console.log(response);
+        console.log(response.data);
         setSurveys(response.data);
         } catch (error) {
         console.error(error);
@@ -52,6 +141,8 @@ function QuestionListPage() {
     };
     fetchData();
   }, []);
+
+  console.log(QuestionService.list());
 
   const header2 = {
     header: "Soru Listesi", to: "/soru-listesi", describe:
@@ -74,9 +165,12 @@ function QuestionListPage() {
     <Layout>
 
         <div className='flex flex-col  gap-10 bg-slate-100 h-full'>
-        <BreadCrumbs header={header2} subtitle={subtitle} />
-        <Table  data={surveys} header={header} useIcon={true} useLabel={false} deleteTableRows={deleteTableRows} editTableRows={handleEditClick}/>
-          </div>
+          <BreadCrumbs header={header2} subtitle={subtitle} />
+          <DataTable  
+            data={surveys} 
+            columns={header} 
+          />
+        </div>
         
         </Layout>
   )
