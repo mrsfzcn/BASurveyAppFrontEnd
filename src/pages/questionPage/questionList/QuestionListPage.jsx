c600d5c8552a9a4d47e2bda72b32ff8036894277
mrsfzcn <mrsfzcn@gmail.com>
2024-01-02 12:34:46 +0300

    Task-1006
    #### QuestionListPage.jsx güncellendi
    
    - SearchBar entegre edildi ve işlevselliği kazandırıldı.
    - Sayfalama özelliği entegre
    edildi ve işlevselliği kazandırıldı.
    - CSS düzenlemesi yapıldı.
    - Tostify eklemesi yapıldı.
    - Gereksiz kod parçaları ve log'lar kaldırıldı.

--- src/pages/questionPage/questionList/QuestionListPage.jsx
+++ src/pages/questionPage/questionList/QuestionListPage.jsx
@@ -1,152 +1,167 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import "./questionListPage.css";
 import { useNavigate } from "react-router-dom";
-import Layout from '../../../components/Layout'
-import QuestionService from '../../../services/QuestionService';
-import BreadCrumbs from '../../../components/BreadCrumbs'
-import DataTable from 'react-data-table-component';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Layout from "../../../components/Layout";
+import QuestionService from "../../../services/QuestionService";
+import BreadCrumbs from "../../../components/BreadCrumbs";
+import DataTable from "react-data-table-component";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Input from "../../../components/Input";
 
-import EditIcon from '../../user/AllUsers/svg/edit-svg';
-import DeleteIcon from '../../user/AllUsers/svg/delete-svg';
+import EditIcon from "../../user/AllUsers/svg/edit-svg";
+import DeleteIcon from "../../user/AllUsers/svg/delete-svg";
 
 function QuestionListPage() {
-
   const successNotify = (string) => toast.success(string);
-  const errorNotify = (string)=> toast.error(string);
-  const warnNotify = (string)=> toast.warn(string);
+  const errorNotify = (string) => toast.error(string);
+  const warnNotify = (string) => toast.warn(string);
 
-  const [surveys, setSurveys] = useState([ //bunun adı neden surveys ? question ile çalışmıyor muyuz?
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [questionList, setQuestionList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [listedItems, setListedItems] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+
+  const [surveys, setSurveys] = useState([
     {
       no: "",
       soru: "",
       tipi: "",
       etiketler: "",
       secenekler: "",
-    }
+    },
   ]);
 
   const header = [
     {
-      name: 'No',
-      selector: row => row.questionOid,
+      name: "No",
+      selector: (row) => row.questionOid,
       sortable: true,
-    }, 
+    },
     {
-      name: 'Soru',
-      selector: row => {
-        if(row.questionType == "Matriks")
-        return row.questionString.split(" $$ ").join(", ")
-        return row.questionString
+      name: "Soru",
+      selector: (row) => {
+        if (row.questionType == "Matriks")
+          return row.questionString.split(" $$ ").join(", ");
+        return row.questionString;
       },
       sortable: true,
-    }, 
+    },
     {
       name: "Soru Tipi",
-      selector: row =>  row.questionType,
+      selector: (row) => row.questionType,
       sortable: true,
-    }, 
+    },
     {
       name: "Etiketler",
-      selector: row => {
-        console.log(row.questionTags);
-        return row.questionTags},
+      selector: (row) => {
+        return row.questionTags;
+      },
       sortable: true,
     },
     {
       name: "Seçenekler",
-      selector: row => {
-        if(Array.isArray(row.questionOptions)){
-          return row.questionOptions.join(", ")
+      selector: (row) => {
+        if (Array.isArray(row.questionOptions)) {
+          return row.questionOptions.join(", ");
         }
-        return row.questionOptions
+        return row.questionOptions;
       },
       sortable: true,
     },
     {
       name: "İşlemler",
-      selector: row => {
-        return <>
-        <div className='flex gap-2'>
-        <button onClick={()=> deleteQuestion(row.questionOid)}>
-          <DeleteIcon className='delete-btn'/>
-        </button>
-        <button onClick={()=> handleEditClick(row)}>
-          {console.log(row)}
-          <EditIcon className='edit-btn'/>
-        </button>
-        </div>
-        </>
-        
+      selector: (row) => {
+        return (
+          <>
+            <div className="flex gap-2">
+              <button onClick={() => deleteQuestion(row.questionOid)}>
+                <DeleteIcon className="delete-btn" />
+              </button>
+              <button onClick={() => handleEditClick(row)}>
+                <EditIcon className="edit-btn" />
+              </button>
+            </div>
+          </>
+        );
       },
       sortable: false,
-    }
+    },
   ];
 
-  const deleteQuestion = async(questionOid) => {
-    let response;
-    try{
-      response = await QuestionService.delete(questionOid);
-      successNotify(questionOid + " No'lu soru başarıyla silindi");
-    } catch(error){
-      errorNotify(error.response.data.exceptionMessage);
-    }
-    if(response.data){
-      setSurveys(
-        surveys.filter(survey => survey.questionOid != questionOid)
-        )
-    }
-  }
-
-  console.log(surveys);
-
-  // To delete selected question
-  const deleteTableRows = async (index, rowData) => {
+  const deleteQuestion = async (questionOid) => {
     let response;
     try {
-      response = await QuestionService.delete(rowData.questionOid);
+      response = await QuestionService.delete(questionOid);
+      successNotify(questionOid + " No'lu soru başarıyla silindi");
     } catch (error) {
       errorNotify(error.response.data.exceptionMessage);
     }
-
     if (response.data) {
-      successNotify(rowData.questionOid + " No'lu soru başarıyla silindi");
-    console.log(response);
-    const rows = [...surveys];
-    rows.splice(index, 1);
-    setSurveys(rows);
-    } else {
-      errorNotify("bir hata meydana geldi");
+      setSurveys(surveys.filter((survey) => survey.questionOid != questionOid));
     }
   };
 
   const navigate = useNavigate();
 
   const handleEditClick = (rowData) => {
-    console.log(rowData);
-    navigate(`/soru-listesi/guncelle/${rowData.questionOid}`, { state: rowData });
+    navigate(`/soru-listesi/guncelle/${rowData.questionOid}`, {
+      state: rowData,
+    });
   };
 
-  // To get all list 
+  // To get all list
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await QuestionService.list()
-        console.log(response.data);
+        const response = await QuestionService.list();
         setSurveys(response.data);
-        } catch (error) {
+      } catch (error) {
         console.error(error);
+        errorNotify(error);
       }
     };
     fetchData();
   }, []);
+  useEffect(() => {
+    QuestionService.list()
+      .then((response) => {
+        setQuestionList(response.data);
+        setFilteredList(response.data);
+        setListedItems(response.data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage));
+      })
+      .catch((error) => {
+        setQuestionList([]);
+        setFilteredList([]);
+        errorNotify("Bağlantı isteği reddedildi. Lütfen sunucu sahibiyle iletişime geçiniz."+ error);
+      });
+  }, []);
 
-  console.log(QuestionService.list());
+  const handleChangePage = (page) => {
+    setCurrentPage(page);
+    setListedItems(filteredList.slice((page - 1) * itemsPerPage, page * itemsPerPage));
+  };
+  const handleChangeRowsPerPage = (newPerPage, page) => {
+    setItemsPerPage(newPerPage);
+    setCurrentPage(page);
+    setListedItems(filteredList.slice((page - 1) * newPerPage, page * newPerPage));
+  };
+  const handleSearch = (e) => {
+    setFilteredList(questionList.filter((item)=>
+    item.questionString.toLowerCase().trim().includes(e.target.value.toLowerCase().trim())
+  ));
+    setListedItems(questionList.filter((item)=>
+    item.questionString.toLowerCase().trim().includes(e.target.value.toLowerCase().trim())
+  ).slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage));
+  };
 
-  const header2 = {
-    header: "Soru Listesi", to: "/soru-listesi", describe:
-      "Soru listesi sayfasına hoşgeldiniz buradan soruları görüntüleyebilirsiniz." };
+  const mainHeader = {
+    header: "Soru Listesi",
+    to: "/soru-listesi",
+    describe:
+      "Soru listesi sayfasına hoşgeldiniz buradan soruları görüntüleyebilirsiniz.",
+  };
   const subtitle = [
     {
       title: "Anasayfa",
@@ -161,19 +176,55 @@ function QuestionListPage() {
       to: "/soru-listesi",
     },
   ];
-    return (
-    <Layout>
 
-        <div className='flex flex-col  gap-10 bg-slate-100 h-full'>
-          <BreadCrumbs header={header2} subtitle={subtitle} />
-          <DataTable  
-            data={surveys} 
-            columns={header} 
-          />
+  const paginationComponentOptions = {
+    rowsPerPageText: 'Satır Sayısı',
+    rangeSeparatorText: 'of',
+    selectAllRowsItem: true,
+    selectAllRowsItemText: 'Tümü',
+};
+  return (
+    <Layout>
+      <div className="background">
+        <div className="allUsersHeaderDiv ">
+          <div style={{ marginLeft: "70px" }}>
+            <BreadCrumbs header={mainHeader} subtitle={subtitle} />
+          </div>
         </div>
-        
-        </Layout>
-  )
+        <div>
+          <div className="list px-8">
+            <div className="flexColumnContainer flex items-center justify-end">
+              <div className="listAra pb-4 ">
+                <span>Ara: </span>
+                <Input className="araButton" onChange={handleSearch} />
+              </div>
+            </div>
+            <div className="flex flex-col  gap-10 bg-slate-100 h-full">
+              <DataTable
+                data={listedItems}
+                columns={header}
+                pagination
+                paginationServer
+                paginationTotalRows={filteredList.length}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                paginationRowsPerPageOptions={[2, 5, 10]}
+                paginationComponentOptions={paginationComponentOptions}
+              />
+            </div>
+            <div className="footer mobile:m-0">
+                <div>
+                  <p style={{ marginBottom: "2rem" }}>
+                    {filteredList.length} sorudan {listedItems.length} tanesi
+                    gösteriliyor.
+                  </p>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
-export default QuestionListPage
+export default QuestionListPage;
